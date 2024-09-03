9badbbf3db53c6248fadeb2f4b9a658ff3f17c74
JohariSahil <sahiljohari04@gmail.com>
2024-09-03 21:21:57 +0530

    added a fix for edit

--- app/api/prompt/[id]/route.js
+++ app/api/prompt/[id]/route.js
@@ -23,7 +23,7 @@ export const PATCH = async (request, { params }) => {
   try {
     await connectToDB();
 
-    const existingPrompt = await prompt.findById(params.id);
+    const existingPrompt = await Prompt.findById(params.id);
     if (!existingPrompt)
       return new Response("Prompt not found", { status: 404 });
 
@@ -31,7 +31,7 @@ export const PATCH = async (request, { params }) => {
     existingPrompt.tag = tag;
 
     await existingPrompt.save();
-    return new Response(JSON.stringify(existingPrompt), { status: 200 });
+    return new Response("Successfully updated the prompt", { status: 200 });
   } catch (error) {
     return new Response("Failed to update prompt", { status: 500 });
   }
